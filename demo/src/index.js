f89033529234d9d7f4de141b0f210e192cc68663
jn.canches <jn.canches@e-deal.com>
2017-10-20 17:24:52 +0200

    Initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,51 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import Card from '../../src'
 
 class Demo extends Component {
-  render() {
-    return <div>
-      <h1>6nimmt-card Demo</h1>
-      <Example/>
-    </div>
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            faceValue: 1,
+            cardWidth: 266
+        };
+
+        this._setFaceValueState = this._setFaceValueState.bind(this);
+        this._setCardWidthState = this._setCardWidthState.bind(this);
+    }
+
+    _setFaceValueState(event) {
+        this.setState({faceValue: event.target.value});
+    }
+
+    _setCardWidthState(event) {
+        this.setState({cardWidth: event.target.value});
+    }
+
+    render() {
+
+        const divStyle = {
+            margin: 'auto',
+            width: '800px'
+        };
+
+        return (
+            <div style={divStyle}>
+                <label>
+                    Face value :
+                    <input type="range" min={0} max={104} value={this.state.faceValue} onChange={this._setFaceValueState}/>
+
+                    Card size (width) :
+                    <input type="range" min={266} max={600} value={this.state.cardWidth} onChange={this._setCardWidthState}/>
+                </label>
+
+                <br/>
+                <br/>
+                <Card faceValue={this.state.faceValue} width={this.state.cardWidth}/>
+            </div>
+        );
+    }
 }
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
 import React, {Component} from 'react'
+import styles from './styles.scss';
+import fontStyle from './assets/fonts/6nimmt/style.scss';
+import classnames from 'classnames';
+
 
 export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+    render() {
+        let weight = this.getWeightFromFaceValue(this.props.faceValue);
+
+        let classes = classnames('card', {
+            'weight-1': weight === 1,
+            'weight-2': weight === 2,
+            'weight-3': weight === 3,
+            'weight-5': weight === 5,
+            'weight-7': weight === 7
+        });
+
+        const divStyle = {
+            '--card-width': this.props.width + 'px',
+            '--card-height': (this.props.width * 1.52) + 'px'
+        };
+
+        return (
+            <div className={classes} style={divStyle}>
+                <div className="header">
+                    <label className="face-value-left">{this.props.faceValue}</label>
+                    weight {weight}
+                    <label className="face-value-right">{this.props.faceValue}</label>
+                </div>
+                <i className="icon-bull"/>
+                <label className="face-value">{this.props.faceValue}</label>
+                <div className="footer">
+                    <label className="face-value-left">{this.props.faceValue}</label>
+                    weight {weight}
+                    <label className="face-value-right">{this.props.faceValue}</label>
+                </div>
+            </div>
+        );
+    }
+
+    getWeightFromFaceValue(faceValue) {
+        var weight = 0;
+        if (faceValue % 11 === 0) {
+            weight += 5;
+        }
+        if (faceValue % 10 === 5) {
+            weight += 2;
+        }
+        if (faceValue % 10 === 0) {
+            weight += 3;
+        }
+        return weight || 1;
+    }
 }
