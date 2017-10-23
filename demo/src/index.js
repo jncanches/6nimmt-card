4877d66650e3f312362643ac1a2d410b5b034113
jn.canches <jn.canches@e-deal.com>
2017-10-23 16:46:58 +0200

    Responsive demo page

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
+import styles from './style.css'
+
 import Card from '../../src'
 
 class Demo extends Component {
@@ -24,35 +26,21 @@ class Demo extends Component {
     }
 
     render() {
-
-        const pageStyle = {
-            width: '100%',
-            padding: '20px'
-        };
-
-        const selectorStyle = {
-            position: 'absolute',
-            width: '200px',
-            padding: '20px'
-        }
-
         const cardContainerStyle = {
-            position: 'relative',
             width: this.state.containerWidth + 'px',
-            margin: 'auto',
-            marginBottom: '33%'
         }
 
         return (
-            <div style={pageStyle}>
-                <div style={selectorStyle}>
-                    Face value :
+            <div className="demo-page">
+                <h1>6 Nimmt card component</h1>
+                <div className="selectors">
+                    <div className="selector-label">Face value :</div>
                     <input type="range" min={1} max={104} value={this.state.faceValue} onChange={this._setFaceValueState}/>
-
-                    Container's width :
+                    <br/>
+                    <div className="selector-label">Container's width :</div>
                     <input type="range" min={200} max={600} value={this.state.containerWidth} onChange={this._setContainerWidthState}/>
                 </div>
-                <div style={cardContainerStyle}>
+                <div className="card-container" style={cardContainerStyle}>
                     <Card faceValue={this.state.faceValue}/>
                 </div>
             </div>
